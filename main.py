5bc9c0d158f2b6e4a6365c6039f61d16ba3d59bf
Aldiyar <aimbetov.ab@gmail.com>
2026-03-19 10:12:11 +0500

    Убрал токен бота в целях безопасности

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = "8709196130:AAHqY0tUxruSWSE34zHGp015oVUnqejX99M"
+API_TOKEN = "YOUR_TOKEN"
+admin_id = 123456789  # Твой телеграм ID
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-admin_id = 123456789
+class OrderState(StatesGroup):
+    waiting_for_quantity = State()
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-
     kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton("Купить товар", callback_data="buy"))
-
     await message.answer("Добро пожаловать в магазин!", reply_markup=kb)
 
 @dp.callback_query_handler(lambda c: c.data == "buy")
 async def buy(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, "Введите количество товара")
+    await OrderState.waiting_for_quantity.set()
 
-@dp.message_handler()
-async def order(message: types.Message):
-
+@dp.message_handler(state=OrderState.waiting_for_quantity)
+async def order(message: types.Message, state: FSMContext):
     text = f"""
 Новый заказ
-User: {message.from_user.username}
+User: @{message.from_user.username}
 Количество: {message.text}
 """
-
     await bot.send_message(admin_id, text)
-
     await message.answer("Заказ отправлен!")
+    await state.finish()
 
 if __name__ == "__main__":
     executor.start_polling(dp)
